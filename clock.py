e4b9e388379fa192a2a1fea4471775813af94fca
Harvey Randall <harvey_randall@me.com>
2020-06-09 14:24:43 +0100

    remove reference to old test method

--- clock.py
+++ clock.py
@@ -2,7 +2,6 @@
 # https://devcenter.heroku.com/articles/clock-processes-python
 from apscheduler.schedulers.background import BlockingScheduler
 from letterboxd import get_diary
-from utils import cronjob
 
 scheduler = BlockingScheduler()
 scheduler.add_job(get_diary, 'cron', day="*", hour=5)
