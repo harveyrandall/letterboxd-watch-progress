e6283dbd975c42f895e0b9147fa54cfdbe10548d
Harvey Randall <harvey_randall@me.com>
2020-06-12 20:27:11 +0100

    add test cronjob for apscheduler && update diary.csv

--- clock.py
+++ clock.py
@@ -1,9 +1,11 @@
 # Implemented with reference to:
 # https://devcenter.heroku.com/articles/clock-processes-python
-from apscheduler.schedulers.background import BlockingScheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
 from letterboxd import get_diary
+from utils import cronjob
 
 scheduler = BlockingScheduler()
-scheduler.add_job(get_diary, 'cron', day="*", hour=5)
+scheduler.add_job(get_diary, 'interval', hours=1)
+scheduler.add_job(cronjob, 'interval', seconds=10)
 
 scheduler.start()
